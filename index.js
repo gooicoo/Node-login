ea70912fbe33323420bce44f6cb863bcba0819f9
Gooicoo <didaclopez93@gmail.com>
2020-02-03 17:51:04 +0100

    ignore

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
 app.set('view engine' , 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }))
 
+var LocalStorage = require('node-localstorage').LocalStorage,
+localStorage = new LocalStorage('./scratch');
+
+
 
 var users = {
-  'paco':'noni',
-  'oscar':'noni',
   'didac':'noni',
 }
 
+
+// parte del login
 app.get('/login', function (req, res) {
   res.render('loginfrom');
 });
@@ -26,6 +31,28 @@ app.post('/login', function (req, res) {
   }
   res.render('loginfromPost' , {user:user} );
 });
+
+
+// parte del registro
+app.get('/registro', function (req, res) {
+  res.render('registro');
+});
+
+app.post('/registro', function (req, res) {
+  var user = req.body.username;
+  var pass = req.body.password;
+
+  if (typeof localStorage === "undefined" || localStorage === null) {
+    var LocalStorage = require ('node-localstorage'). LocalStorage;
+    localStorage = new LocalStorage ('./ scratch');
+  }
+
+  localStorage.setItem('user', 'pass');
+  res.render('registroCorrecto');
+});
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
